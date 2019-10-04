241e1830c644202ca7e7aa6a2aa80d18d6195ee1
regexistaken <32675731+AnkitGupta17134@users.noreply.github.com>
2019-10-04 22:21:47 +0530

    Even Odd Bisection

--- main.java
+++ main.java
@@ -1,1 +1,19 @@
-
+import java.util.*;
+class main
+{
+  public static void main(String[] args)
+  {
+    Scanner jk = new Scanner(System.in);
+    String io = jk.nextLine();
+    System.out.println("Input String "+io);
+    if (io.length%2==0)
+    {
+      Collections.reverse(io);
+      System.out.println("Output Reverse "+io);
+    }
+    else
+    {
+      System.out.println("Output Reverse "+io);
+    }
+  }
+}
