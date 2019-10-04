54a0bfd9cc8d96aab7c07e58f77d50deed9ff934
regexistaken <32675731+AnkitGupta17134@users.noreply.github.com>
2019-10-04 22:12:48 +0530

    Input of Image

--- main.java
+++ main.java
@@ -1,1 +1,10 @@
-
+import java.util.*;
+class main
+{
+  public static void main(String[] args)
+  {
+    Scanner jk = new Scanner(System.in);
+    String io = jk.nextLine();
+    System.out.println("Input String "+io);
+  }
+}
