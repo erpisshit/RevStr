f9ea7576eeb1daac473c26bfac7e96efe99a5494
regexistaken <32675731+AnkitGupta17134@users.noreply.github.com>
2019-10-04 22:14:31 +0530

    Reverssing the string

--- main.java
+++ main.java
@@ -1,1 +1,12 @@
-
+import java.util.*;
+class main
+{
+  public static void main(String[] args)
+  {
+    Scanner jk = new Scanner(System.in);
+    String io = jk.nextLine();
+    System.out.println("Input String "+io);
+    Collections.reverse(io);
+    System.out.println("Output Reverse "+io);
+  }
+}
